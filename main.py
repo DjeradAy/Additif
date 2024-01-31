f490513f8ea058622543d037b4348123ac2d4f88
DjeradAy <153507923+DjeradAy@users.noreply.github.com>
2024-01-31 19:23:20 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from data_preparation import DataPreparation
 from Additif import AdditifModel
 
-
-csv_path = "./number of travelers.csv"
+csv_path = r"C:\Users\Djera\OneDrive\Bureau\data_1+\regression (partir de ce code pour le projet à rendre le 31-01-2024)\Additif\vente_maillots_de_bain(1).csv"
 data_preparation_object = DataPreparation(csv_path)
-regression_object = Regression(data_preparation_object)
+regression_object = AdditifModel(data_preparation_object)
+regression_object.show_model_predictions()
 
-# data_preparation_object.show_graph()
+
+
+#YANIS , AYMEN DIA1
